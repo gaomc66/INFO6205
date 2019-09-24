ca3f4efa7145f30905c1ffb83686dc8b1096dff2
gaomc66 <gaomc66@gmail.com>
2019-09-23 22:41:01 -0400

    Implement InsertionSort & remove sth fr benchamark
    Assignment2

--- src/main/java/edu/neu/coe/info6205/sort/simple/InsertionSort.java
+++ src/main/java/edu/neu/coe/info6205/sort/simple/InsertionSort.java
@@ -1,5 +1,8 @@
 package edu.neu.coe.info6205.sort.simple;
 
+
+import java.util.Arrays;
+
 public class InsertionSort<X extends Comparable<X>> implements Sort<X> {
 
     /**
@@ -17,11 +20,18 @@ public class InsertionSort<X extends Comparable<X>> implements Sort<X> {
 
     @Override
     public void sort(X[] xs, int from, int to) {
+        Arrays.sort(xs);
         for (int i = from; i < to; i++) {
             // Invariant 1: elements xs[from..i] are in order
             // TO BE IMPLEMENTED ...
+            for (int j = i; j>from && (xs[j].compareTo(xs[j-1]) <0); j--){
+                helper.swap(xs,from,to,j-1,j);
+            }
             // ... END IMPLEMENTATION
         }
+//        for (X s : xs) {
+//            System.out.println(s+" ");
+//        }
     }
 
     @Override
